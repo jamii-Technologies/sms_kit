91e5e3608f1d8a5121633ce8b03aa5515f037771
glaszig <glaszig@gmail.com>
2014-02-08 04:14:47 +0100

    custom log formatting

--- lib/sms_kit/logging.rb
+++ lib/sms_kit/logging.rb
@@ -1,12 +1,26 @@
 module SmsKit
   module Logging
 
-    attr_accessor :logger
-
     def logger
       @logger ||= begin
         require 'logger'
-        Logger.new STDOUT
+        Logger.new(STDOUT).tap do |l|
+          l.formatter ||= Logger::Formatter.new
+          l.formatter.extend Formatter
+        end
+      end
+    end
+
+    def logger= l
+      @logger = l.tap do |l|
+        l.formatter ||= Logger::Formatter.new
+        l.formatter.extend Formatter
+      end
+    end
+
+    module Formatter
+      def call severity, datetime, progname, msg
+        '[SmsKit] ' + super(severity, datetime, progname, msg)
       end
     end
 
