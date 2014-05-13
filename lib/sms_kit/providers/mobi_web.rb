814f3460c276f697d9ad4e53cbff928e78193acf
glaszig <glaszig@gmail.com>
2014-05-13 23:13:51 +0200

    mobiweb: extended, proper error handling

--- lib/sms_kit/providers/mobi_web.rb
+++ lib/sms_kit/providers/mobi_web.rb
@@ -5,12 +5,33 @@ module SmsKit
 
     HTTP_ENDPOINT = "http://217.118.27.5/bulksms/bulksend.go"
 
+    ERROR_CODES = {
+      100 => 'Temporary Internal Server Error. Try again later',
+      101 => 'Authentication Error',
+      102 => 'No credits available',
+      103 => 'MSIDSN (phone parameter) is invalid or prefix is not supported',
+      104 => 'Tariff Error',
+      105 => 'You are not allowed to send to that destination/country',
+      106 => 'Not Valid Route number or you are not allowed to use this route',
+      107 => 'No proper Authentication (IP restriction is activated)',
+      108 => 'You have no permission to send messages through HTTP API',
+      109 => 'Not Valid Originator',
+      110 => 'You are not allowed to send (Routing not available) or Reseller is trying to send while not allowed',
+      111 => 'Invalid Expiration date or Expiration Date is less than 30 minutes than the date of SMS submission',
+      999 => 'Invalid HTTP request'
+    }
+
     def deliver
       response = get params
       status   = response.body[/([a-z]+)(\d+)?/i, 1]
-      sms_id   = response.body[/([a-z]+)(\d+)?/i, 2]
+      code     = response.body[/([a-z]+)(\d+)?/i, 2]
 
-      status == "OK" ? sms_id.to_i : nil
+      if 'ERROR' == status
+        @error_code = code.to_i
+        @error_message = ERROR_CODES[code.to_i]
+      end
+
+      status == "OK" ? code.to_i : nil
     end
 
     def params
--- test/mobi_web_test.rb
+++ test/mobi_web_test.rb
@@ -9,21 +9,28 @@ end
 module SmsKit
   class MobiWebTest < MiniTest::Test
 
+    def vcr_options
+      { match_requests_on: [
+        VCR.request_matchers.uri_without_params(:username, :password)
+      ]}
+    end
+
     def text_message
       { text: 'foo bar', to: 12345 }
     end
 
     def test_deliver
-      options = { match_requests_on: [
-        VCR.request_matchers.uri_without_params(:username, :password)
-      ]}
-
-      VCR.use_cassette 'mobi_web_success', options do
+      VCR.use_cassette 'mobi_web_success', vcr_options do
         assert_equal 123, MobiWeb.deliver(text_message)
       end
+    end
 
-      VCR.use_cassette 'mobi_web_error', options do
-        assert_nil MobiWeb.deliver
+    def test_error
+      VCR.use_cassette 'mobi_web_error', vcr_options do
+        provider = MobiWeb.new
+        assert_nil provider.deliver
+        assert_equal 100, provider.error_code
+        assert_equal 'Temporary Internal Server Error. Try again later', provider.error_message
       end
     end
 
