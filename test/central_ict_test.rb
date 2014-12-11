f1c9a0a2e12abebc201e7e5fb6a874fe71e6fdbd
glaszig <glaszig@gmail.com>
2014-12-11 22:34:06 +0100

    param tests for CentralICT and MobiWeb

--- test/central_ict_test.rb
+++ test/central_ict_test.rb
@@ -10,9 +10,22 @@ end
 module SmsKit
   class CentralICTTest < MiniTest::Test
 
+    def text_message
+      { text: 'foo bar', to: 12345 }
+    end
+
+    def test_params
+      message = CentralICT.new text_message
+      assert_equal text_message[:to], message.params[:dst]
+      assert_equal CentralICT.config.sender, message.params[:src]
+      assert_equal text_message[:text], message.params[:body]
+      assert_equal 'message_sender', message.params[:call]
+      assert_equal 'SMS', message.params[:type]
+    end
+
     def test_deliver
       VCR.use_cassette 'central_ict/success' do
-        assert CentralICT.deliver text: 'foo bar', to: 12345
+        assert CentralICT.deliver text_message
       end
     end
 
