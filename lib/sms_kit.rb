0cd057b265e4dedff10eaa8883209de37aab8923
glaszig <glaszig@gmail.com>
2015-01-29 00:32:02 +0100

    adding railtie to configure proper rails-based logger

--- lib/sms_kit.rb
+++ lib/sms_kit.rb
@@ -2,6 +2,7 @@ require 'sms_kit/version'
 require 'sms_kit/utils'
 require 'sms_kit/delivery'
 require 'sms_kit/logging'
+require 'sms_kit/railtie' if defined?(Rails)
 
 module SmsKit
   extend Utils
